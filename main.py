248ee8702f9f7837c985b154fc82b80ab24dc305
hugomacielads <70041844+hugomacielads@users.noreply.github.com>
2021-08-28 21:05:29 +0000

    Lista de números pares e ímpares

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 import random
 
 print('Hello word!!')
-var = [1, 2, 3, 4, 5]
+# var = [1, 2, 3, 4, 5]
 list = []
 
-for i in range(10):
-  a = random.randint(1, 10)
-  list.append(a)
+listPar = []
+listImpar = []
+cont = 0
+
+while cont <= 20:
+  for i in range(100):
+    a = random.randint(1, 20)
+    if a in list:
+      continue
+    else:
+      #a = a ** 2
+      list.append(a)
+      cont += cont + 1
+
+list.sort()
 
 for i in list:
   if (i % 2):
     print('numero ímpar: ' + str(i))
+    listImpar.append(i)
   else:
     print('Número par: ' + str(i))
+    listPar.append(i)
 
+print("\nLista aleatória de números pares", listPar)
+print("Lista aleatória de números ímpares", listImpar)
